494e35206250bae202637a2c38fbb94062f94e17
domicioribon <domicioribon@gmail.com>
2024-09-28 20:05:48 -0300

    exemplo de carregamento de post

---
+++ pages/blog/index.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import axios from "axios";
+const list = ref([]);
+
+onMounted(() => {
+  loadPosts();
+});
+
+function loadPosts() {
+  axios.get("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => {
+      list.value = response.data;
+    });
+    .catch((error) => {
+      console.error(error);
+    });
+}
+</script>
+
+<template>
+  <div>
+    <button @click="loadPosts">Atualizar</button>
+    <div v-for="post in list" :key="post.id">
+      <h2>{{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<style lang="css"></style>
